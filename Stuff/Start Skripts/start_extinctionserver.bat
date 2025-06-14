0fc94d232196ff6f9d6b82aa844ec85f1ed50c54
Tim Milker <127841608+tmilka@users.noreply.github.com>
2025-06-14 15:01:44 +0200

    Duplikate gelöscht

--- Stuff/Start Skripts/start_extinctionserver.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-start ShooterGame\Binaries\Win64\ShooterGameServer.exe ScorchedEarth_P?listen?SessionName=TheBoisARK?ServerPassword=Milchmann01?ServerAdminPassword=AdminWassermann01?Port=7779?QueryPort=27016?RCONPort=7780 -clusterid=TheBoisARK -ClusterDirOverride="C:\Users\Tim\Desktop\ARK_Cluster_Server\ClusterStorage" -nobattleye -server -log
-pause
